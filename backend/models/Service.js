30d8fda8030f1534fc787ccb5da628516540ba88
Ahmed Yasser Singap <130315264+singap88a@users.noreply.github.com>
2026-02-01 21:01:05 +0200

    first commit

---
+++ backend/models/Service.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const ServiceSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Service', ServiceSchema);
