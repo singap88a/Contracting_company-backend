77d62b4014421cf60f458ad935adbf778968c690
Ahmed Yasser Singap <130315264+singap88a@users.noreply.github.com>
2026-02-02 13:10:10 +0200

    ttt

--- backend/server.js
+++ backend/server.js
@@ -32,3 +32,5 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
